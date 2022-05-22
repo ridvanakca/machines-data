47f330248038be946493c3a6a843af0413b9145c
Rıdvan Akca <akca.ridvann@gmail.com>
2022-05-22 17:20:57 +0300

    filtered data is added

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,8 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { data } from "../src/machines";
 
 export default (req: VercelRequest, res: VercelResponse) => {
+  const filteredData = data.filter((group) => group.name.toLowerCase().includes(req.query.query as any));
   res.setHeader("Access-Control-Allow-Credentials", `true`);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  return res.json({ machines: data });
+  return res.json({ machines: filteredData });
 };
