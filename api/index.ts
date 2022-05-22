73ba47188ca42f3f99ff992a6296c1e97b5175ea
Rıdvan Akca <akca.ridvann@gmail.com>
2022-05-22 17:23:30 +0300

    comment

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import { data } from "../src/machines";
 
 export default (req: VercelRequest, res: VercelResponse) => {
   const filteredData = data.filter((group) => group.name.toLowerCase().includes(req.query.query as any));
+  // filteredData
   res.setHeader("Access-Control-Allow-Credentials", `true`);
   res.setHeader("Access-Control-Allow-Origin", "*");
   return res.json({ machines: filteredData });
